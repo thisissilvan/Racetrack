3f8c64c6aee66f7721b1c5ec5b21e5eafa5de8da
kuendkun <kuendkun@students.zhaw.ch>
2020-03-05 11:26:51 +0100

    feat: add to constructor of class Launcher
    #29

--- src/main/java/ch/zhaw/pm2/racetrack/Launcher.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Launcher.java
@@ -9,7 +9,15 @@ public class Launcher {
     void runGame(TextIO textIO, TextTerminal<?> terminal, Game game) {
         boolean running = true;
         while (game.getWinner()== -1 && running) {
-
+            terminal.println("-------------------------------------------------------------");
+            terminal.println("Current player " + game.getCarId(game.getCurrentCarIndex()));
+            terminal.println("Current velocity " + game.getCarVelocity(game.getCurrentCarIndex()));
+            terminal.println("Change velocity to one of the following: ");
+            Direction acceleration = textIO.newEnumInputReader(Direction.class).read("What would you like to choose?");
+            game.doCarTurn(acceleration);
+            if(game.getWinner()== -1){
+                game.switchToNextActiveCar();
+            }
         }
         terminal.println("-------------------------------------------------------------");
         terminal.println("Congratulation " + game.getWinner());
