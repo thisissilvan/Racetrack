a12b8f7b4a250772193283595a4393dba0c1b04b
kuendkun <kuendkun@students.zhaw.ch>
2020-03-05 11:25:17 +0100

    feat: add new class Launcher

---
+++ src/main/java/ch/zhaw/pm2/racetrack/Launcher.java
@@ -0,0 +1,12 @@
+package ch.zhaw.pm2.racetrack;
+import static ch.zhaw.pm2.racetrack.PositionVector.*;
+
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextTerminal;
+import java.awt.*;
+
+public class Launcher {
+
+}
+
+
