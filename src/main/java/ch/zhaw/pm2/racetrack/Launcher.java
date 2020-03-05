b934cd164433b36758d4667ccd67ba910378902f
kuendkun <kuendkun@students.zhaw.ch>
2020-03-05 11:25:51 +0100

    feat: add constructor to class Launcher

--- src/main/java/ch/zhaw/pm2/racetrack/Launcher.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Launcher.java
@@ -6,7 +6,15 @@ import org.beryx.textio.TextTerminal;
 import java.awt.*;
 
 public class Launcher {
+    void runGame(TextIO textIO, TextTerminal<?> terminal, Game game) {
+        boolean running = true;
+        while (game.getWinner()== -1 && running) {
 
+        }
+        terminal.println("-------------------------------------------------------------");
+        terminal.println("Congratulation " + game.getWinner());
+        terminal.println("You have won!");
+    }
 }
 
 
