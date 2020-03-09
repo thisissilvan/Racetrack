e0e2f69f4f9fbcdecec927a46979560e82d1d5ce
kuendkun <kuendkun@students.zhaw.ch>
2020-03-09 16:29:40 +0100

    feat: move GameLauncher to Runner #29

--- src/main/java/ch/zhaw/pm2/racetrack/Runner.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Runner.java
@@ -1,15 +1,16 @@
 package ch.zhaw.pm2.racetrack;
 
+import ch.zhaw.pm2.racetrack.exception.InvalidTrackFormatException;
+
+import java.io.FileNotFoundException;
+
 /**
  * This class is responsible for the game setup and the initialisation of the game.
  * It receives a display-instance of the game-launcher which is responsible for the text-output and
  * could be replaced with a java-fx class.
  */
 public class Runner {
-    private static GameLauncher gameLauncher;
+    Display display = new Display();
+    Game game;
 
-    public static void main(String[] args) {
-        gameLauncher = new GameLauncher();
-        //gameLauncher.run();
-    }
 }
