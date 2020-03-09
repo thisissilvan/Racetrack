40189a1a21e08cd549e5eb0abf17cb1a31e812ba
luethsil <luethsil@students.zhaw.ch>
2020-03-09 16:31:14 +0100

    feat: add method currentTurn() #31

--- src/main/java/ch/zhaw/pm2/racetrack/Display.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Display.java
@@ -25,7 +25,17 @@ public class Display {
         return new File(config.getTrackDirectory(),trackFileName);
     }
 
-    
+    public PositionVector.Direction currentTurn(char id, PositionVector velocity){
+        terminal.println("Current player " + id);
+        terminal.println("Current velocity " + velocity);
+        terminal.println("Change velocity to one of the following: ");
+        String format = "%-13s%s%n";
+        terminal.printf(format,"DOWN_LEFT", "UP_LEFT", "LEFT");
+        terminal.printf(format,"DOWN", "UP", "NONE");
+        terminal.printf(format,"DOWN_RIGHT", "UP_RIGHT", "RIGHT");
+        PositionVector.Direction acceleration = textIO.newEnumInputReader(PositionVector.Direction.class).read("What would you like to choose?");
+        return acceleration;
+    }
 
 
 
