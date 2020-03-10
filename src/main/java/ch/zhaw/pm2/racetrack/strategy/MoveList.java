0a660ad7b948d608ae190e0406dad450b22852b3
luethsil <luethsil@students.zhaw.ch>
2020-03-10 17:47:09 +0100

    feat: add constructor to MoveList #30

--- src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveList.java
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveList.java
@@ -4,7 +4,14 @@ import ch.zhaw.pm2.racetrack.PositionVector;
 
 
 public class MoveList implements MoveStrategy {
-    
+
+    private final PositionVector[] moveList;
+
+    public MoveList(PositionVector[] moveList) {
+        this.moveList = moveList;
+    }
+
+    // TODO get next positions
     @Override
     public PositionVector.Direction nextMove() {
         return null;
