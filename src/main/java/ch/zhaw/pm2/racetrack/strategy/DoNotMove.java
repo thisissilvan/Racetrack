50434a5a13537754bb245fe1eab1a4301afe5739
kuendkun <kuendkun@students.zhaw.ch>
2020-03-10 15:56:36 +0100

    feat: new Class DoNotMove

---
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/DoNotMove.java
@@ -0,0 +1,10 @@
+package ch.zhaw.pm2.racetrack.strategy;
+
+import ch.zhaw.pm2.racetrack.PositionVector;
+
+public class DoNotMove implements MoveStrategy {
+    @Override
+    public PositionVector.Direction nextMove() {
+        return null;
+    }
+}
