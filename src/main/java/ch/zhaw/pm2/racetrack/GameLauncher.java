7d7448fc86864300b9bec01fefb31a0d5796e33c
luethsil <luethsil@students.zhaw.ch>
2020-03-10 21:57:04 +0100

    feat: GameLauncher add class #29

--- src/main/java/ch/zhaw/pm2/racetrack/GameLauncher.java
+++ src/main/java/ch/zhaw/pm2/racetrack/GameLauncher.java
@@ -1,15 +1,10 @@
 package ch.zhaw.pm2.racetrack;
-import static ch.zhaw.pm2.racetrack.PositionVector.*;
-
-import org.beryx.textio.TextIO;
-import org.beryx.textio.TextTerminal;
 
 public class GameLauncher {
-    public static void main(String[] args) {
-        // Game konfigurieren und setup
-        // Game Logik starten
-        // run() methode aufrufen
-    }
+    Display display = new Display();
+    Game game = new Game();
+    Runner runner = new Runner();
+
 
 }
 
