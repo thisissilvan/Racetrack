6526fa35d88bccaba2f04118f442a37fe52b5b03
luethsil <luethsil@students.zhaw.ch>
2020-03-10 21:57:46 +0100

    feat: GameLauncher add main method and helper methods #29

--- src/main/java/ch/zhaw/pm2/racetrack/GameLauncher.java
+++ src/main/java/ch/zhaw/pm2/racetrack/GameLauncher.java
@@ -5,6 +5,23 @@ public class GameLauncher {
     Game game = new Game();
     Runner runner = new Runner();
 
+    public static void main(String[] args) {
+        GameLauncher gameLauncher = new GameLauncher();
+        gameLauncher.gameInit();
+        gameLauncher.gameStart();
+    }
+
+    private void gameInit() {
+        display.welcomeMesseage();
+        //Read Track-File here
+        //Load Track file
+    }
+
+    private void gameStart() {
+        while (game.getWinner() == Game.NO_WINNER) {
+            runner.run();
+        }
+    }
 
 }
 
