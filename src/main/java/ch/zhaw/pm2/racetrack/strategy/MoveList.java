b1cd4b948524c3b3c79e054b0df43b5ca5c8b0f1
luethsil <luethsil@students.zhaw.ch>
2020-03-10 17:46:39 +0100

    feat: add body MoveList #30

---
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveList.java
@@ -0,0 +1,12 @@
+package ch.zhaw.pm2.racetrack.strategy;
+
+import ch.zhaw.pm2.racetrack.PositionVector;
+
+
+public class MoveList implements MoveStrategy {
+    
+    @Override
+    public PositionVector.Direction nextMove() {
+        return null;
+    }
+}
