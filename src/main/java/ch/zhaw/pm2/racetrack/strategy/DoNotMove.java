34295100ba1296522f358b0ca951eb1a9b519060
kuendkun <kuendkun@students.zhaw.ch>
2020-03-10 16:03:45 +0100

    docs : add JavaDoc to class and method

--- src/main/java/ch/zhaw/pm2/racetrack/strategy/DoNotMove.java
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/DoNotMove.java
@@ -2,7 +2,17 @@ package ch.zhaw.pm2.racetrack.strategy;
 
 import ch.zhaw.pm2.racetrack.PositionVector;
 
+/**
+ * This class implements MoveStrategy and defines a strategy with no movement.
+ * A car with this strategy always remains in the same position as in the beginning.
+ */
+
 public class DoNotMove implements MoveStrategy {
+
+    /**
+     * The car never moves. It's Direction is NONE which equals (0,0)
+     * @return always returns NONE
+     */
     @Override
     public PositionVector.Direction nextMove() {
         return PositionVector.Direction.NONE;
