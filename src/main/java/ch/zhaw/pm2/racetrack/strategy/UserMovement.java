5e9fc66bcc18fe0cb602e123a3dcf615c39ef42a
kuendkun <kuendkun@students.zhaw.ch>
2020-03-10 16:41:58 +0100

    docs : add JavaDoc to class and method

--- src/main/java/ch/zhaw/pm2/racetrack/strategy/UserMovement.java
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/UserMovement.java
@@ -3,9 +3,17 @@ package ch.zhaw.pm2.racetrack.strategy;
 import ch.zhaw.pm2.racetrack.Display;
 import ch.zhaw.pm2.racetrack.PositionVector;
 
+/**
+ * This class implements MoveStrategy and defines a strategy where the user enters the direction at each turn.
+ */
 public class UserMovement implements MoveStrategy{
 
     Display display = new Display();
+
+    /*
+     * The user enters the direction through display.
+     * @return the users choice on direction
+     */
 
     @Override
     public PositionVector.Direction nextMove() {
