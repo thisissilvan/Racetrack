e0a734f76273f9f6c3069f3da7813236809dd332
kuendkun <kuendkun@students.zhaw.ch>
2020-03-12 10:55:28 +0100

    fix: invalid user-input    #30

--- src/main/java/ch/zhaw/pm2/racetrack/Display.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Display.java
@@ -32,16 +32,22 @@ public class Display {
 
     public File readInputFile(){
         File newFile=null;
-        try {
-            String trackFileName = textIO.newStringInputReader().read();
-            newFile = new File(config.getTrackDirectory(), trackFileName);
-            for(int index = 0; index < config.getTrackDirectory().list().length;index++){
-                if(!Arrays.asList(config.getTrackDirectory().list()).get(index).equals(newFile.toString())){
-                    throw new IllegalArgumentException();
+        boolean valid=false;
+        while(!valid) {
+            try {
+                String trackFileName = textIO.newStringInputReader().read();
+                for (int index = 0; index < config.getTrackDirectory().list().length; index++) {
+                    if (Arrays.asList(config.getTrackDirectory().list()).get(index).equals(trackFileName)) {
+                        newFile = new File(config.getTrackDirectory(), trackFileName);
+                        valid=true;
+                    }
                 }
+                if(!valid){
+                    throw new  IllegalArgumentException();
+                }
+            } catch (IllegalArgumentException e) {
+                terminal.println("Please enter the name of a valid track");
             }
-        }catch (IllegalArgumentException e){
-            terminal.println("Please enter the name of a valid track");
         }
         return newFile;
     }
@@ -66,18 +72,24 @@ public class Display {
         terminal.printf(format,"2", "UserMovement");
         terminal.printf(format,"3", "MoveList");
         int number = textIO.newIntInputReader().read();
-        try{
-            if (number==1){
-                strategy=new DoNotMove();
-            }else if(number==2){
-                strategy= new UserMovement();
-            }else if(number ==3){
-                //strategy = new MoveList();
-            }else{
-                throw new IllegalArgumentException();
+        boolean valid=false;
+        while(!valid) {
+            try {
+                if (number == 1) {
+                    strategy = new DoNotMove();
+                    valid = true;
+                } else if (number == 2) {
+                    strategy = new UserMovement();
+                    valid = true;
+                } else if (number == 3) {
+                    //strategy = new MoveList();
+                    valid = true;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            } catch (IllegalArgumentException e) {
+                terminal.println("Please enter a value between 1 - 3");
             }
-        }catch (IllegalArgumentException e){
-            terminal.println("Please enter a value between 1 - 3");
         }
         return strategy;
     }
