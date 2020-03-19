525a3289979fda0a9631be0c89d209fda401726d
luethsil <luethsil@students.zhaw.ch>
2020-03-19 13:07:24 +0100

    fix: PR change request, number of players #31

--- src/main/java/ch/zhaw/pm2/racetrack/Display.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Display.java
@@ -33,13 +33,6 @@ public class Display {
 
     }
 
-    public int numberOfPlayers() {
-        terminal.println("How many players will play this game?\n\nEnter a number from 2 - 9:");
-        if (textIO.newIntInputReader().read() >= 2 || textIO.newIntInputReader().read() <= 9) {
-        }
-        return textIO.newIntInputReader().read();
-    }
-
     public boolean playANewGame() {
         boolean playANewGame = false;
         terminal.println("Would you like to play another round? \n\n (y/n)");
